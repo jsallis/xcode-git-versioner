6fc89e0a8093cb0992ac77369ab0871c7a2abe89
Jason Sallis <jsallis@gmail.com>
2008-10-02 10:54:09 -0600

    initial commit

---
+++ README.markdown
@@ -0,0 +1,13 @@
+xcode-git-versioner
+===================
+
+xcode-git-versioner is an Xcode auto-versioning script written in Ruby which updates your Info.plist file CFBundleVersion to the latest git revision number
+
+Usage
+-----
+
+1. Right-click the target you want to add the versioning phase to (usually the target that builds your app)
+2. Select: Add -> New Build Phase -> New Run Script Build Phase
+3. Specify /usr/bin/ruby as the Shell for the script
+4. Paste the script body into the Script text area
+5. Ensure that the build phase is at the end of the target's list of build phases 
---
+++ xcode-git-versioner.rb
@@ -0,0 +1,19 @@
+# Xcode auto-versioning script for git
+#
+# Based on original Perl script by Marcus S. Zarra and Matt Long at
+#   (http://www.cimgf.com/2008/04/13/git-and-xcode-a-git-build-number-script/)
+
+# fail if not run from Xcode
+raise "Must be run from Xcode" unless ENV['XCODE_VERSION_ACTUAL']
+
+info_plist = "#{ENV['BUILT_PRODUCTS_DIR']}/#{ENV['WRAPPER_NAME']}/Contents/Info.plist"
+
+# Get the current git revision hash
+revision = `/usr/local/bin/git rev-parse --short HEAD`.chomp!
+
+# open Info.plist and set the CFBundleVersion value to the revision hash
+lines = IO.readlines(info_plist).join
+lines.gsub! /(<key>CFBundleVersion<\/key>\n\t<string>).*?(<\/string>)/, "\\1#{revision}\\2"
+
+# overwrite the original Info.plist file with our modified version
+File.open(info_plist, 'w') {|f| f.puts lines}
