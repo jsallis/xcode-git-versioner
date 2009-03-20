7ec8ccaf8c0d2b2a79cb2bf1fff370bdbafb6bb8
Jason Sallis <jsallis@gmail.com>
2009-03-19 18:53:37 -0600

    now reporting to the user which plist file was updated and to what revision

--- xcode-git-versioner.rb
+++ xcode-git-versioner.rb
@@ -3,7 +3,7 @@
 # Based on original Perl script by Marcus S. Zarra and Matt Long at
 #   (http://www.cimgf.com/2008/04/13/git-and-xcode-a-git-build-number-script/)
 
-# fail if not run from Xcode
+# Fail if not run from Xcode
 raise "Must be run from Xcode" unless ENV['XCODE_VERSION_ACTUAL']
 
 info_plist = "#{ENV['BUILT_PRODUCTS_DIR']}/#{ENV['INFOPLIST_PATH']}"
@@ -11,9 +11,12 @@ info_plist = "#{ENV['BUILT_PRODUCTS_DIR']}/#{ENV['INFOPLIST_PATH']}"
 # Get the current git revision hash
 revision = `/usr/local/bin/git rev-parse --short HEAD`.chomp!
 
-# open Info.plist and set the CFBundleVersion value to the revision hash
+# Open Info.plist and set the CFBundleVersion value to the revision hash
 lines = IO.readlines(info_plist).join
 lines.gsub! /(<key>CFBundleVersion<\/key>\n\t<string>).*?(<\/string>)/, "\\1#{revision}\\2"
 
-# overwrite the original Info.plist file with our modified version
+# Overwrite the original Info.plist file with our updated version
 File.open(info_plist, 'w') {|f| f.puts lines}
+
+# Report to the user
+puts "CFBundleVersion in #{info_plist} set to '#{revision}'"
